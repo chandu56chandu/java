1083ae164041b9f1b0b05f65f5e922d1fe24a8b8
chandu56chandu <138272995+chandu56chandu@users.noreply.github.com>
2023-09-02 12:17:35 +0530

    Create CmdLineArgs.java

---
+++ CmdLineArgs.java
@@ -0,0 +1,11 @@
+import java.io.*;
+public class CmdLineArray{
+    public static void main(String args[]){
+    int x = Integer.parseInt(args[0]);
+    int y = Integer.parseInt(args[1]);
+    int z = x+y;
+    System.out.println("SUm of " +x+ " and " +y+ " is " +z);
+    }
+}
+
+   output:SUm of -5 and 4 is -1
