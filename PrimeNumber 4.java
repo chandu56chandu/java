ea58a8d835aeb0459f5bbe6cac54315f20b91ea4
ChanduRavipati56 <138272995+chandu56chandu@users.noreply.github.com>
2023-09-23 12:51:37 +0530

    Add files via upload

---
+++ PrimeNumber 4.java
@@ -0,0 +1,26 @@
+public class Prime {
+
+    public static void main(String[] args) {
+
+        int low = 560, high = 600;
+
+        while (low < high) {
+            boolean flag = false;
+
+            for(int i = 2; i <= low/2; ++i) {
+                // condition for nonprime number
+                if(low % i == 0) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag && low != 0 && low != 1)
+                System.out.print(low + " ");
+
+            ++low;
+        }
+    }
+}
+OUTPUT:
+563 569 571 577 587 593 599 
