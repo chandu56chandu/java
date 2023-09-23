f80a5b4c60313574b64646f91ff5a6d341f1430f
ChanduRavipati56 <138272995+chandu56chandu@users.noreply.github.com>
2023-09-23 12:49:45 +0530

    Add files via upload

---
+++ IfElseExample.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class IfElseExample{
+    public static void main(String args[]){
+        int x=10;
+        if(x>10)
+        {
+            System.out.println("GOOD MORNING");
+        }
+        else
+        {
+            System.out.println("GOOD AFTERNOON");
+        }
+    }
+}
+
+OUTPUT:
+GOOD AFTERNOON
