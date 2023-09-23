741ba22e6b6a77168fb1cc48c0f5fab652188195
ChanduRavipati56 <138272995+chandu56chandu@users.noreply.github.com>
2023-09-23 12:48:24 +0530

    Add files via upload

---
+++ ElseIfExample.java
@@ -0,0 +1,22 @@
+import java.io.*;
+public class ElseIfExample{
+    public static void main(String args[]){
+        int x=20;
+        int y=20;
+        if(x>10)
+        {
+            System.out.println("GOOD MORNING");
+        }
+        else if(y>10)
+        {
+            System.out.println("GOOD AFTERNOON");
+        }
+        else
+        {
+            System.out.println("GOOD EVENING");
+        }
+    }
+}
+
+OUTPUT:
+GOOD MORNING
